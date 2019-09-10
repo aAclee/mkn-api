29090d29df41ffcdf4df202cabe457e41b495a7c
Calvin Lee <clee421@gmail.com>
2019-09-10 19:16:51 -0400

    feat: boilerplate server code

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,7 +1,28 @@
 package main
 
-import "fmt"
+import (
+	"log"
+
+	"github.com/aaclee/mkn-api/pkg/http"
+	"github.com/aaclee/mkn-api/pkg/logger"
+)
 
 func main() {
-	fmt.Println("Hello, world!")
+	// Setting up logger
+	logger := logger.CreateLogger()
+
+	// Create server configs
+	config := http.Config{
+		Log:  logger,
+		Port: 8000,
+	}
+
+	// Create server
+	server := http.CreateServer(config)
+
+	// Listen and Serve
+	err := server.ListenAndServe()
+	if err != nil {
+		log.Fatalf("Server could not start on port: %d\n", config.Port)
+	}
 }
