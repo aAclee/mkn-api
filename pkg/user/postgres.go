bd2dd7dbbf70720a8cc8ebdec99e9d57a5e241f9
Calvin Lee <clee421@gmail.com>
2019-09-18 18:58:05 -0400

    feat: add user postgres repository

---
+++ pkg/user/postgres.go
@@ -0,0 +1,39 @@
+package user
+
+import (
+	"database/sql"
+)
+
+// IModel represents the model interface
+type IModel interface {
+	GetEmail() string
+}
+
+// PostgresRepository is the backing user repository invoked by services
+type PostgresRepository struct {
+	psql *sql.DB
+}
+
+// CreatePostgresRepository creates an instance of the user repository struct
+func CreatePostgresRepository(db *sql.DB) *PostgresRepository {
+	return &PostgresRepository{
+		psql: db,
+	}
+}
+
+// FindUserByEmail finds a user by email
+func (r *PostgresRepository) FindUserByEmail(email string) (IModel, error) {
+	row := r.psql.QueryRow(
+		`SELECT id, uuid, email FROM users
+		WHERE email = $1`,
+		email,
+	)
+
+	model := &Model{}
+	err := row.Scan(&model.ID, &model.UUID, &model.Email)
+	if err != nil {
+		return nil, err
+	}
+
+	return model, nil
+}
