0086cd5379624f62b06ca23c06ac42435545ceb0
Calvin Lee <clee421@gmail.com>
2019-09-20 23:14:17 -0400

    chore: change users to players in migrations, add admin column

--- db/migrations/20190916140943_create_players_table.up.sql
+++ db/migrations/20190916140943_create_players_table.up.sql
@@ -1,7 +1,8 @@
-CREATE TABLE IF NOT EXISTS users (
+CREATE TABLE IF NOT EXISTS players (
   id SERIAL PRIMARY KEY,
   uuid UUID NOT NULL UNIQUE,
   email text NOT NULL UNIQUE,
+  admin BOOLEAN DEFAULT false,
   created_at timestamptz DEFAULT now(),
   updated_at timestamptz DEFAULT now()
 );
