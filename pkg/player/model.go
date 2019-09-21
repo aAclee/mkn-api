7cab85e992021fdb327168040b8c5342e000cf8e
Calvin Lee <clee421@gmail.com>
2019-09-20 23:23:45 -0400

    chore: rename user pkg to player pkg

--- pkg/player/model.go
+++ pkg/player/model.go
@@ -1,20 +1,20 @@
-package user
+package player
 
 import "github.com/google/uuid"
 
-// Model representats the collection of user information
+// Model representats the collection of player information
 type Model struct {
 	ID    int       `json:"id"`
 	UUID  uuid.UUID `json:"uuid"`
 	Email string    `json:"email"`
 }
 
-// GetEmail returns the user email
+// GetEmail returns the player email
 func (m *Model) GetEmail() string {
 	return m.Email
 }
 
-// GetUUID returns the user email
+// GetUUID returns the player email
 func (m *Model) GetUUID() uuid.UUID {
 	return m.UUID
 }
