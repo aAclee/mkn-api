e57843c69a07d69030eb03338583f2d000698cd3
Calvin Lee <clee421@gmail.com>
2019-09-22 13:15:36 -0400

    feat: player service calls player repo for create

--- pkg/player/service.go
+++ pkg/player/service.go
@@ -1,20 +1,27 @@
 package player
 
-import "github.com/google/uuid"
+type playerRepository interface {
+	CreatePlayer(email string) (IModel, error)
+}
 
 // Service is the backing player service invoked by HTTP/REST handlers
-type Service struct{}
+type Service struct {
+	player playerRepository
+}
 
 // CreateService creates an instance of the player service struct
-func CreateService() *Service {
-	return &Service{}
+func CreateService(pr playerRepository) *Service {
+	return &Service{
+		player: pr,
+	}
 }
 
 // CreatePlayer creates a new player
 func (s *Service) CreatePlayer(email string) (IModel, error) {
-	return &Model{
-		ID:    -1,
-		UUID:  uuid.New(),
-		Email: email,
-	}, nil
+	player, err := s.player.CreatePlayer(email)
+	if err != nil {
+		return nil, err
+	}
+
+	return player, nil
 }
