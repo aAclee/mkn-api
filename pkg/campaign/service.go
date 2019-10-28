47395b2008b4bb1c880168ad686db65148ca14b8
Calvin Lee <clee421@gmail.com>
2019-10-28 00:08:02 -0400

    feat: add FindCampaignByID to service layer

--- pkg/campaign/service.go
+++ pkg/campaign/service.go
@@ -2,6 +2,7 @@ package campaign
 
 type campaignRepository interface {
 	CreateCampaign(email string) (IModel, error)
+	FindCampaignByID(id string) (IModel, error)
 }
 
 // Service is the backing campaign service invoked by HTTP/REST handlers
@@ -19,6 +20,16 @@ func CreateService(cr campaignRepository) *Service {
 // CreateCampaign creates a new campaign
 func (s *Service) CreateCampaign(name string) (IModel, error) {
 	campaign, err := s.campaign.CreateCampaign(name)
+	if err != nil {
+		return nil, err
+	}
+
+	return campaign, nil
+}
+
+// FindCampaignByID finds a campaign by :id
+func (s *Service) FindCampaignByID(id string) (IModel, error) {
+	campaign, err := s.campaign.FindCampaignByID(id)
 	if err != nil {
 		return nil, err
 	}
