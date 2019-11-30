a4235f4537699ed7822d90447a0e39e42a1cd8d6
Calvin Lee <clee421@gmail.com>
2019-11-29 21:28:00 -0500

    feat: character service

--- pkg/character/service.go
+++ pkg/character/service.go
@@ -2,6 +2,7 @@ package character
 
 type characterRepository interface {
 	CreateCharacter(c *Model) (IModel, error)
+	FindCharacterByID(id string) (IModel, error)
 }
 
 // Service is the backing character service invoked by HTTP/REST handlers
@@ -19,6 +20,16 @@ func CreateService(cr characterRepository) *Service {
 // CreateCharacter creates a new character
 func (s *Service) CreateCharacter(c *Model) (IModel, error) {
 	character, err := s.character.CreateCharacter(c)
+	if err != nil {
+		return nil, err
+	}
+
+	return character, nil
+}
+
+// FindCharacterByID finds a character by :id
+func (s *Service) FindCharacterByID(id string) (IModel, error) {
+	character, err := s.character.FindCharacterByID(id)
 	if err != nil {
 		return nil, err
 	}
